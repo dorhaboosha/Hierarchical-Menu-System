6b196330f1c46fd9b8c6dd541bcd6aaa7f592368
Dor Haboosha <134487550+dorhaboosha@users.noreply.github.com>
2026-02-08 17:24:53 +0200

    Adding documentation to IActionExector file

--- Menus.Interfaces/IActionExector.cs
+++ Menus.Interfaces/IActionExector.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace Menus.Interfaces
 {
+    /// <summary>
+    /// Defines a contract for objects that can execute an action.
+    /// </summary>
     public interface IActionExector
     {
+        /// <summary>
+        /// Performs the action associated with this executor.
+        /// </summary>
         void Execute();
     }
 }
